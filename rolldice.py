489fcaaa7a0d78e2bda981cc68e7e23a79330ebe
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-10-31 14:48:37 +0100

    Commented out things, then took them back again

--- rolldice.py
+++ rolldice.py
@@ -1,7 +1,7 @@
 from random import randint
 
-# print (randint(1,6))
-# rollagain = raw_input("Would you like to roll again?")
+print (randint(1,6))
+rollagain = raw_input("Would you like to roll again?")
 
 while rollagain == "yes":
 	print (randint(1,6))
