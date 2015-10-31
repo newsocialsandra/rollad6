4c6b99ba62d39bf63c9db1380da8570a23042ab1
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-10-31 14:27:59 +0100

    Added while loop

--- rolldice.py
+++ rolldice.py
@@ -1,2 +1,9 @@
 from random import randint
-print (randint(1,6))
+
+# print (randint(1,6))
+# rollagain = raw_input("Would you like to roll again?")
+
+while rollagain == "yes":
+	print (randint(1,6))
+	rollagain = raw_input("Would you like to roll again?")
+	
